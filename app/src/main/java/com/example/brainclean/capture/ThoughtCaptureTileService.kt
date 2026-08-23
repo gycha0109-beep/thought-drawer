04aa2c8c06bc46d41c013de5f69b99279ce40a47
gycha0109-beep <gycha0109@gmail.com>
2026-08-23 16:14:38 +0900

    Add Quick Settings capture tile

---
+++ app/src/main/java/com/example/brainclean/capture/ThoughtCaptureTileService.kt
@@ -0,0 +1,46 @@
+package com.example.brainclean.capture
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
+import android.service.quicksettings.TileService
+import com.example.brainclean.QuickAddActivity
+import com.example.brainclean.model.CaptureSource
+
+class ThoughtCaptureTileService : TileService() {
+    override fun onClick() {
+        super.onClick()
+
+        val launchQuickAdd = Runnable {
+            val intent = Intent(this, QuickAddActivity::class.java)
+                .putExtra(
+                    QuickAddActivity.EXTRA_CAPTURE_SOURCE,
+                    CaptureSource.QUICK_SETTINGS.name
+                )
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                val pendingIntent = PendingIntent.getActivity(
+                    this,
+                    QUICK_ADD_REQUEST_CODE,
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                )
+                startActivityAndCollapse(pendingIntent)
+            } else {
+                @Suppress("DEPRECATION")
+                startActivityAndCollapse(intent)
+            }
+        }
+
+        if (isLocked) {
+            unlockAndRun(launchQuickAdd)
+        } else {
+            launchQuickAdd.run()
+        }
+    }
+
+    companion object {
+        private const val QUICK_ADD_REQUEST_CODE = 42_001
+    }
+}
